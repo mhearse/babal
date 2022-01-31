f45e9387b6838c82d9438752c68a7e42c36a1107
Matt Hersant <matt_hersant@yahoo.com>
2022-01-31 00:18:01 -0600

    Update babal.py

--- babal.py
+++ babal.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python
 
+import Tkinter as tk
+import time
+
+root = tk.Tk()
+canvas = tk.Canvas(root)
+canvas.pack()
+
+canvas.create_polygon(
+    [
+        50,50,
+        100,50,
+        125,200,
+        25,200
+    ],
+    tags = 'babal'
+)
+
+for _ in range(50):
+    canvas.move('babal', 0, 5)
+    canvas.update()
+    time.sleep(0.05)
+
+root.mainloop()
